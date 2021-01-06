cd076655fd4a66fff37d63d6ec3ab031540a78d2
yuchanleeme <yuchanlee.me@gmail.com>
2021-01-06 15:59:06 +0900

    bottom navigator

--- App.js
+++ App.js
@@ -23,32 +23,16 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
-  );
-}
-
-function SearchScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Serch!</Text>
-    </View>
-  );
-}
-
-function SettingsScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+import HomeScreen from './src/Screen/HomeScreen';
+import NotificationScreen from './src/Screen/NotificationScreen';
+import ProfileScreen from './src/Screen/ProfileScreen';
+import RankingScreen from './src/Screen/RankingScreen';
+import WriteScreen from './src/Screen/WriteScreen';
 
 const Tab = createBottomTabNavigator();
 
@@ -56,38 +40,37 @@ export default function App() {
   return (
     <>
     <NavigationContainer>
-      <Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? 'ios-file-tray' : 'ios-file-tray-outline';
+            } else if (route.name === 'Ranking') {
+              iconName = focused ? 'ios-hand-right' : 'ios-hand-right-outline';
+            } else if (route.name === 'Write') {
+              iconName = focused ? 'ios-add-circle' : 'ios-add-circle-outline';
+            } else if (route.name === 'Notification') {
+              iconName = focused ? 'ios-chatbubble' : 'ios-chatbubble-outline';
+            } else if (route.name === 'Profile') {
+              iconName = focused ? 'ios-happy' : 'ios-happy-outline';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}>
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Search" component={SearchScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Ranking" component={RankingScreen} />
+        <Tab.Screen name="Write" component={WriteScreen} />
+        <Tab.Screen name="Notification" component={NotificationScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
     </>
   );
 }
-
-// const App: () => React$Node = () => {
-//   return (
-//     <>
-//       <StatusBar barStyle="dark-content" />
-//       <SafeAreaView>
-//         <ScrollView
-//           contentInsetAdjustmentBehavior="automatic"
-//           style={styles.scrollView}>
-//           <View style={styles.body}>
-            // <View style={styles.sectionContainer}>
-            //   <Text style={styles.sectionTitle}>Learn More</Text>
-            //   <Text style={styles.sectionDescription}>
-            //     Read the docs to discover what to do next:
-            //   </Text>
-            // </View>
-//             <LearnMoreLinks />
-//           </View>
-//         </ScrollView>
-//       </SafeAreaView>
-//     </>
-//   );
-// };
 
 const styles = StyleSheet.create({
   scrollView: {
