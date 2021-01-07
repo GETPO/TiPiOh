941644ce87d266b62dfb8700fd4b82d8bea78f13
injeChoi <2015jasonc@gmail.com>
2021-01-07 20:09:07 +0900

    Firebase DB에 있는 모든 데이터 조회

---
+++ TiPiOh_Server/src/test/java/com/TiPiOh_Server/TiPiOh_Server/TiPiOhServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TiPiOh_Server.TiPiOh_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiPiOhServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
