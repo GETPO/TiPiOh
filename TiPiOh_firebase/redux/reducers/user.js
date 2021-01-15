a76dd04e057c95bf0d691751fe78264f5d6ffae1
injeChoi <2015jasonc@gmail.com>
2021-01-15 23:34:07 +0900

    검색 기능 추가 + 검색한 유저의 프로필 보기 기능 구현

--- TiPiOh_firebase/redux/reducers/user.js
+++ TiPiOh_firebase/redux/reducers/user.js
@@ -1,11 +1,26 @@
+import { USER_STATE_CHANGE, USER_POSTS_STATE_CHANGE } from "../constants/index";
+
 const initialState = {
-    currentUser: null
+    currentUser: null,
+    posts: []
 }
 
 // action으로 DB에서 데이터를 불러와서 fetching 하고, 데이터를 reducer로 전달하여 state를 업데이트 한다.
 export const user = (state = initialState, action) => {
-    return {
-        ...state, 
-        currentUser: action.currentUser
+    switch (action.type) {
+        case USER_STATE_CHANGE:
+            return {
+                ...state, 
+                currentUser: action.currentUser
+            }
+        
+        case USER_POSTS_STATE_CHANGE:
+            return {
+                ...state, 
+                posts: action.posts
+            }
+        default:
+            return state;
     }
+    
 }
