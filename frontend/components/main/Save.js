764fe7dae6beca077186c1a32c94c65dfb06cab8
yuchanleeme <yuchanlee.me@gmail.com>
2021-01-20 03:38:17 +0900

    Write 스크린 수정

--- frontend/components/main/Save.js
+++ frontend/components/main/Save.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react'
-import { View, TextInput, Image, Button, Text, StyleSheet } from "react-native";
-import { List } from 'react-native-paper';
+import { View,  Image, StyleSheet,} from "react-native";
+import { List, ProgressBar, Menu, TextInput, Button} from 'react-native-paper';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Datepicker, Layout, Text, IconRegistry ,Input, Icon } from '@ui-kitten/components';
+import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import firebase from 'firebase';
 require("firebase/firestore")
 require("firebase/firebase-storage")
@@ -11,12 +14,15 @@ export default function Save(props) {
     // 이미지 설명 -> caption, setCaption 함수로 react에게 입력한 값 전달
     const [caption, setCaption] = useState("")
     const [imageURI, setImageURI] = useState("")
-    const [time, setTime] = useState("")
-    const [expanded, setExpanded] =useState(true)
+    const [time, setTime] = useState(new Date())
+    const [saveTime, setsaveTime] = useState(0);
+
+    const now = new Date();
 
     useEffect(() => {
         setImageURI(props.route.params.image)
-    }, [imageURI])
+    }, [imageURI, saveTime])
+
     const uploadImage = async() => {
         const uri = props.route.params.image;
         // 이미지가 저장될 Firebase의 Storage 경로
@@ -27,11 +33,13 @@ export default function Save(props) {
         const task = firebase.storage().ref().child(childPath).put(blob);
         // 업로드 한 이미지 크기가 어느 정도인지 확인하는 기능
         const taskProgress = snapshot => {
+            setsaveTime(0.3)
             //console.log(`transferred: ${snapshot.bytesTransferred}`)
         }
         // 업로드 한 이미지를 public하게 볼 수 있게 하는 기능
         const taskCompleted = () => {
             task.snapshot.ref.getDownloadURL().then((snapshot) => {
+                setsaveTime(1)
                 savePostData(snapshot);     // Storage 말고 firestore DB에 이미지 업로드 했다는 post 저장하기
                 //console.log(snapshot)
             })
@@ -53,50 +61,46 @@ export default function Save(props) {
                 downloadURL,
                 caption,
                 likesCount: 0,
-                creation: firebase.firestore.FieldValue.serverTimestamp()
+                creation: firebase.firestore.FieldValue.serverTimestamp(),
+                time
             }).then((function () {
                 props.navigation.popToTop() // 이미지 업로드를 하고 나면 Main page로 돌아가는 기능
             }))
     }
 
-    const handlePress = () => setExpanded(!expanded);
 
     return (
-
-        <View style={sytles.container}>
-            <View style={sytles.ImageView}>
-             <Image
-                style={sytles.Imagestyle}
-                source={{uri:imageURI}}/>
-            </View>
-            <View style={sytles.contents}>
-                <List.Section title={<Text style={{fontSize:10}}>Time </Text>}>
-                    <List.Accordion
-                        title={<Text style={{fontSize:10}}>Season</Text>}
-                        // left={props => <List.Icon {...props} size={10} icon="folder" />}
-                        expanded={expanded}
-                        onPress={handlePress}
-                        >
-                        <List.Item title="Spring" />
-                        <List.Item title="Summer" />
-                        <List.Item title="Fall" />
-                        <List.Item title="Winter" />
-                    </List.Accordion>
-                </List.Section>
-                <List.Section title ={<Text style={{fontSize:10}}>Place </Text>}>
-                    <List.Accordion
-                        title={<Text style={{fontSize:10}}>Place</Text>}
-                        >
-                            <List.Item title="Here"/>
-                        </List.Accordion>
-                </List.Section>
-                <TextInput
-                    placeholder="Write a Caption ..."
-                    onChangeText={(caption) => setCaption(caption)}
-                />
-                <Button title="Save" onPress={() => uploadImage()} />
-            </View>
-        </View>
+        <>
+            <IconRegistry icons={EvaIconsPack} />
+            <ApplicationProvider {...eva} theme={eva.light}>
+                <View style={sytles.container}>
+                <ProgressBar progress={saveTime}/>
+                    <View style={sytles.ImageView}>
+                    <Image
+                        style={sytles.Imagestyle}
+                        source={{uri:imageURI}}/>
+                    </View>
+                    <View style={{flex:1 , flexDirection:'row'}}>
+                        <Datepicker
+                            label='Time'
+                            min={new Date(1900)}
+                            max={new Date(now.getFullYear(), now.getMonth(), now.getDate())}
+                            size='small'
+                            date={time}
+                            onSelect={nextDate => setTime(nextDate)}
+                        />
+                    </View>
+                </View>
+                <View style={{flexDirection:'row'}}>
+                    <TextInput
+                        style = {{width:'85%'}}
+                        label="Comment"
+                        onChangeText={(caption) => setCaption(caption)}
+                    />
+                    <Button style = {{width:'10%'}} icon="upload" mode="outlined" onPress={() => uploadImage()}/>
+                </View>
+            </ApplicationProvider>
+        </>
     )
 }
 
@@ -105,10 +109,11 @@ const sytles = StyleSheet.create({
         flex: 1,
         padding: 10,
         backgroundColor: 'white',
+        marginTop: 30
     },
     ImageView: {
         width:'100%',
-        height:'50%',
+        height:'48%',
         justifyContent: 'center',
         alignItems: 'center',
     },
