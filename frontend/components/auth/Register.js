1e26bb4da67b7fd7a0a39730a80a53d023086034
yuchanleeme <yuchanlee.me@gmail.com>
2021-01-23 01:33:05 +0900

    회원가입 스크린 수정

--- frontend/components/auth/Register.js
+++ frontend/components/auth/Register.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
-import { View, Button, TextInput } from 'react-native';
+import ReactNative, { View, SafeAreaView, StyleSheet } from 'react-native';
+import { TextInput, Button, Headline } from 'react-native-paper'
 import firebase from 'firebase';
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 
 export class Register extends Component {
     constructor(props) {
@@ -8,15 +10,19 @@ export class Register extends Component {
         this.state = {
             email: '',
             password: '',
-            name: ''
+            name: '',
+            confirmPassword: ''
         }
         this.onSignUp = this.onSignUp.bind(this);
     }
 
     // Register 화면에서 Sign Up 버튼을 누르면 호출되는 함수 
     onSignUp() {
-        const {email, password, name} = this.state;
-
+        const {email, password, name, confirmPassword} = this.state;
+        if (password !== confirmPassword) {
+            alert("Password Does Not Match!")
+            return;
+        }
         // firestore의 authentication 메뉴에 새로운 유저들 등록
         firebase.auth().createUserWithEmailAndPassword(email, password)
         .then((result) => {                                 // 유저 생성에 성공했으면
@@ -26,35 +32,86 @@ export class Register extends Component {
                 name,
                 email
             })
-            console.log(result);
         })
         .catch((error) => {
             console.log(error);
         })
     }
+
+    _scrollToInput = (reactNode) => {
+        if(this.scroll.props){
+            this.scroll.props.scrollToFocusedInput(reactNode)
+        }
+    }
+
     render() {
         return (
-            <View>
-                <TextInput 
-                    placeholder="name"
-                    onChangeText={(inputName) => this.setState({name: inputName})}
-                />
-                <TextInput 
-                    placeholder="email"
-                    onChangeText={(inputEmail) => this.setState({email: inputEmail})}
-                />
-                <TextInput 
-                    placeholder="password"
-                    secureTextEntry={true}
-                    onChangeText={(inputPassword) => this.setState({password: inputPassword})}
-                />
-                <Button
-                    onPress={() => this.onSignUp()}
-                    title="Sign Up"
-                />
-            </View>
+            <KeyboardAwareScrollView style={styles.default} innerRef={ref => {
+                this.scroll = ref
+              }}>
+                <Headline
+                    style={styles.header}
+                >Register</Headline>
+                <View style={{margin: 20}}>
+                    <TextInput 
+                        style={{marginBottom: 10}}
+                        label="Name"
+                        placeholder="Name"
+                        left={<TextInput.Icon name= "account-circle-outline"/>}
+                        onChangeText={(inputName) => this.setState({name: inputName})}
+                    />
+                    <TextInput 
+                        style={{marginBottom: 10}}
+                        label="Email"
+                        placeholder="email"
+                        left={<TextInput.Icon name= "email-outline"/>}
+                        onChangeText={(inputEmail) => this.setState({email: inputEmail})}
+                    />
+                    <TextInput 
+                        style={{marginBottom: 10}}
+                        label="Password"
+                        placeholder="password"
+                        left={<TextInput.Icon name= "lock-outline"/>}
+                        secureTextEntry={true}
+                        onChangeText={(inputPassword) => this.setState({password: inputPassword})}
+                    />
+                    <TextInput 
+                        label="Confirm Password"
+                        placeholder="Confirm password"
+                        left={<TextInput.Icon name= "lock-outline"/>}
+                        secureTextEntry={true}
+                        onChangeText={(inputPassword) => this.setState({confirmPassword: inputPassword})}
+                    />
+                </View>
+                <View style={{flexDirection:'row'}}>
+                    <Button
+                        style={styles.button}
+                        icon="login"
+                        mode="outlined"
+                        onPress={() => this.onSignUp()}>
+                    Register
+                    </Button>
+                </View>
+            </KeyboardAwareScrollView>
         )
     }
 }
+const styles = StyleSheet.create({
+    header: {
+        padding: 30,
+        alignSelf: "center",
+        fontSize: 30,
+        marginBottom: 1,
+    },
+    default: {
+        marginTop: 50,
+        padding: 10
+    },
+    button: {
+        flex: 1,
+        margin: 15,
+        width: 100,
+    }
+})
 
 export default Register
